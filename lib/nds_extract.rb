06c025c1c2365f4a145bbd2f6e0108af290da1ef
Zeshan Raja <zraja4402@gmail.com>
2019-12-01 22:36:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,8 +3,6 @@ require 'directors_database'
 
 def directors_totals(nds)
 
-#pp nds[0][:movies][0][:worldwide_gross]
-#pp nds.count 
 total = 0 
 row_index = 0 
 
@@ -25,31 +23,6 @@ while director_counter < nds.length do
   director_counter += 1 
 end 
   
- result 
-end
+result 
+end 
 
-# nds[0][:movies][0] gives me four keys: title, studio, worldwide gross, and release year 
-# nds[0][:movies][0][:worldwide_gross] gives me the gross for that movie
-# I need to make a loop that cycles through the movies and adds the worldwide_gross to the total 
-
-
-# grand_total = 0
-# row_index = 0
-# while row_index < vm.length do
-#   column_index = 0
-#   while column_index < vm[row_index].length do
-#     inner_len = vm[row_index][column_index].length
-#     inner_index = 0
-#     while inner_index < inner_len do
-#       # Explanation!
-#       # vm[row][column][spinner]
-#       # spinner is full of Hashes with keys :price and :name
-#       grand_total += vm[row_index][column_index][inner_index][:price]
-#       inner_index += 1
-#     end
-#     column_index += 1
-#   end
-#   row_index += 1
-# end
- 
-# p grand_total #=> 1192
